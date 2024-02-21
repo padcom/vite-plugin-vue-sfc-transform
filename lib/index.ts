5bac91865c02f5ab50226e2790aa6a8f613ab439
Matthias Hryniszak <padcom@gmail.com>
2024-02-21 20:58:22 +0100

    Code cleanup and added export of the import utilities

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import { plugin } from './sfc-transform-plugin'
 
 export * from './sfc-transform-plugin'
+export * from './ts-import-extractor'
 
 /**
  * Exporting the plugin as default export for easier and cleaner use
